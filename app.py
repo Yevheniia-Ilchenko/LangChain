9a88c522bcd85d2e0d1d1015e54d3adba04f87c9
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-11-26 15:21:40 +0200

    Create app with OpenAI and Streamlit UI which answer on simple questions

---
+++ app.py
@@ -0,0 +1,35 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response by user queries"),
+        ('user', "Questions: {question}")
+    ]
+)
+
+st.title("Langchain DEmo with LLM")
+input_text = st.text_input("What you want to ask me?")
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
+
+
+
+
